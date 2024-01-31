62c3618353dd089b351875e708797f65d1e6bd9a
DESKTOP-BO4ASTP\Ken <maplena0608@gmail.com>
2024-01-31 09:08:48 +0800

    test while true

--- testansible.py
+++ testansible.py
@@ -1,4 +1,10 @@
 
+import time
 
-with open("test.txt", "a") as file:
-    file.write("hehe")
+while True:
+
+    with open("test.txt", "a") as file:
+        file.write(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+        file.write("\n")
+    time.sleep(1)
+
